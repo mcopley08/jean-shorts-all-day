a10312f9f7f5149fe714cd1a717568c02b9a5050
Michael <mcopley08@comcast.net>
2015-05-28 01:14:56 -0400

    trying to fixit feliz

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+ini_set('allow_url_fopen', 1);
 
 $requested_url = "http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=" + $_GET['schoolName'] + "&profsid=&query=" + $_GET['firstName'] + "+" + $_GET['lastName'];
 $rate_my_professor = file_get_contents($requested_url);
