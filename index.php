e31d329024226fadc1227e4a4dd4ebee90c848f6
Michael <mcopley08@comcast.net>
2015-05-28 00:59:13 -0400

    making it a https proxy

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+$requested_url = "https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=" + $_GET['schoolName'] + "&profsid=&query=" + $_GET['firstName'] + "+" + $_GET['lastName'];
+$rate_my_professor = file_get_contents($requested_url);
+echo $rate_my_professor;
+
+?>
